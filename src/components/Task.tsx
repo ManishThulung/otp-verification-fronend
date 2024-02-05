222a31abac132c6e17747183548e7ddb3028b5e0
Manish Thulung <manish.thulung58@gmail.com>
2024-02-05 15:59:07 +0545

    storybook setup

---
+++ src/components/Task.tsx
@@ -0,0 +1,46 @@
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}: any) {
+  return (
+    <div className={`list-item ${state} bg-red-700 p-10`}>
+      <label
+        htmlFor="checked"
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+
+      <label htmlFor="title" aria-label={title} className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          placeholder="Input title"
+        />
+      </label>
+
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span>HELLO</span>
+        </button>
+      )}
+    </div>
+  );
+}
